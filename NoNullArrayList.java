44ac89e7b1d7f6442549129441aa410947aeaf4d
ElizaKnapp <eknapp20@stuy.edu>
2020-12-10 20:40:08 -0500

    set is done

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -24,6 +24,13 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     super.add(index, element);
   }
 
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("You cannot set null values");
+    }
+    return super.set(index, element);
+  }
+
 
 
 }
