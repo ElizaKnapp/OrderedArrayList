fb3a90f3b392b8b03e80cf526d253eb16bacf7f2
ElizaKnapp <eknapp20@stuy.edu>
2020-12-10 21:06:47 -0500

    changed find index to a helper function

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -3,18 +3,26 @@ import java.util.*;
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
 
   public boolean add(T element) {
+    super.add(findIndex(element), element);
+    return true;
+  }
+
+  private int findIndex(T element) {
     int index = size() - 1;
     for (int i = 0; i < size() - 1; i++) {
       if (element.compareTo(get(i)) <= 0 && element.compareTo(get(i + 1)) >= 0) {
         index = i;
       }
     }
-    super.add(index, element);
-    return true;
+    return index;
   }
 
   public void add(int index, T element) {
     add(element);
+  }
+
+  public T set(int index, T element) {
+
   }
 
 }
