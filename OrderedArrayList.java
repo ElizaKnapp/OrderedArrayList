c4e28a60b2ab7bfe866e2fb20067aa38b2dcde89
ElizaKnapp <eknapp20@stuy.edu>
2020-12-13 19:38:49 -0500

    found problem in if else statements and fixed code thanks to hw discussion

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -11,17 +11,22 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public boolean add(T element) {
-    super.add(findPlace(element), element);
+    if (element == null) {
+      throw new IllegalArgumentException("You cannot add null values");
+    }
+    if (size() > 0 && element.compareTo(super.get(size() - 1)) >= 0) {
+      super.add(element);
+    }
+    else {
+      super.add(findPlace(element), element);
+    }
     return true;
   }
 
   private int findPlace(T element) {
-    if (element == null) {
-      throw new IllegalArgumentException("You cannot add null values");
-    }
     int index = 0;
     for (int i = 0; i < size() - 1; i++) {
-      if (get(i).compareTo(element) <= 0 && element.compareTo(get(i + 1)) <= 0) {
+      if (get(i).compareTo(element) <= 0 && get(i + 1).compareTo(element) >= 0) {
         index = i + 1;
       }
     }
